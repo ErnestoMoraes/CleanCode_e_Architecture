0ff83871e0730ccd775c149351ea3249fabd2d35
ErnestoMoraes <joseernestosantosmoraes@gmail.com>
2022-12-21 21:22:39 -0300

    5-condições confusas e aninhadas, 6-números mágicos

--- src/calculateRide.v2.ts
+++ src/calculateRide.v2.ts
@@ -1,43 +1,35 @@
-function isOvernight(date: any) {
-    return date.getHours() >= 22 || date.getHours <= 6;
-}
+const OVERNIGTH_FARE = 3.90;
+const OVERNIGTH_SUNDAY_FARE = 5;
+const SUNDAY_FARE = 2.90;
+const NORMAL_FARE = 2.10;
+const OVERNIGTH_START = 22;
+const OVERNIGTH_END = 6;
+const MIN_FARE = 10;
 
-function isSunday(date: any) {
+function isOvernight(date: Date) {
+    return date.getHours() >= OVERNIGTH_START || date.getHours() <= OVERNIGTH_END;
+}
+function isSunday(date: Date) {
     return date.getDay() === 0;
 }
 
 export function calculateRide (segments: { distance: number; date: Date; }[]) {
     let fare = 0;
     for (const segment of segments) {
-        if(segment.distance != null && segment.distance != undefined && typeof segment.distance === "number" && segment.distance > 0) {
-            if(segment.date != null && segment.date != undefined && segment.date instanceof Date && segment.date.toString() !== "Invalid Date") {
-                if(isOvernight(segment.date)) {
-                    if(!isSunday(segment.date)) {
-                        fare += segment.distance * 3.90;
-                    }
-                    else {
-                        fare += segment.distance * 5;
-                    }
-                } else {
-                    if(isSunday(segment.date)){
-                        fare += segment.distance * 2.90;
-                    }else{
-                        fare += segment.distance * 2.10;
-                    }
-                }
-
-            } else {
-                return -2;
-            }
-        
-        } else {
-             return -1;
+        if(!segment.distance || typeof segment.distance !== "number" || segment.distance < 0) return -1;
+        if(!segment.date || !(segment.date instanceof Date) || segment.date.toString() === "Invalid Date") return -2;
+        if(isOvernight(segment.date) && !isSunday(segment.date)) {
+           fare += segment.distance * OVERNIGTH_FARE;
+        }
+        if(isOvernight(segment.date) && isSunday(segment.date)) {
+            fare += segment.distance * OVERNIGTH_SUNDAY_FARE;
+        }
+        if(!isOvernight(segment.date) && isSunday(segment.date)) {
+            fare += segment.distance * SUNDAY_FARE
+        }
+        if(!isOvernight(segment.date) && !isSunday(segment.date)) {
+            fare += segment.distance * NORMAL_FARE;
         }
     }
-    if(fare < 10) {
-        return 10;
-    }
-    else {
-        return fare;
-    }
+    return (fare < MIN_FARE) ? MIN_FARE : fare;
 }
