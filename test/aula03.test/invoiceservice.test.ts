586302ac406f781900dd684cf90bc79b54576e83
ErnestoMoraes <joseernestosantosmoraes@gmail.com>
2023-01-12 14:55:50 -0300

    Concluindo Test Patterns - Stub!

--- test/aula03.test/invoiceservice.test.ts
+++ test/aula03.test/invoiceservice.test.ts
@@ -9,7 +9,7 @@ test('Deve testar o calculo da fatura', async function(){
     const purchaseRepositoryDatabaseSub = sinon.stub(PurchaseRepositoryDatabase.prototype, 'getPurchases').returns(Promise.resolve([new Purchase('123412341324', 100, 'USD')])); 
     const invoiceService = new InvoiceServiceImpl();
     const total = await invoiceService.calculate('123412341234', 9, 2023);
-    expect(total).toBe(690);
+    expect(total).toBe(300);
     currencyGatewayHttpStub.restore();
     purchaseRepositoryDatabaseSub.restore();
 });
