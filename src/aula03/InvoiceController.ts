768ec8d75427f406eb6d8e805100a9d3a1106886
ErnestoMoraes <joseernestosantosmoraes@gmail.com>
2023-01-12 10:16:49 -0300

    Codigo Inical nada perfeito

--- src/aula03/InvoiceController.ts
+++ src/aula03/InvoiceController.ts
@@ -3,8 +3,11 @@ import pgp from "pg-promise";
 import axios from "axios";
 const app = express();
 app.get('/cards/:cardNumber/invoices', async function(req, res) {
+    const date = new Date()
+    const month = date.getMonth() + 1;
+    const year = date.getFullYear();
     const connection = pgp()('postgres://postgres:123456@localhost:5432/app');
-    const purchases = await connection.query('select * from ernesto.purchase', []);
+    const purchases = await connection.query('select * from ernesto.purchase where card_number = $1 and extract(month from date)::integer = $2 and extract(year from date)::integer = $3', [req.params.cardNumber, month, year]);
     const response = await axios.get('http://localhost:3000/currencies');
     const currency = response.data;
     let total = 0;
